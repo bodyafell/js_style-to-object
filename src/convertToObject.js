fd82b2b01b33e3e8e94f2eb271dc8853215e81d6
Bohdan <felbohdan@gmail.com>
2026-03-25 20:13:45 +0200

    add task solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,28 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  let attributePairs = sourceString.split(';');
+
+  attributePairs = attributePairs.filter((pair) => pair.trim() !== '');
+  attributePairs = attributePairs.map((pair) => pair.split(':'));
+
+  attributePairs = attributePairs
+    .map((pair) => {
+      if (pair.length === 2) {
+        return [pair[0].trim(), pair[1].trim()];
+      }
+
+      return null;
+    })
+    .filter((pair) => pair !== null);
+
+  const result = attributePairs.reduce((acc, pair) => {
+    acc[pair[0]] = pair[1];
+
+    return acc;
+  }, {});
+
+  return result;
 }
 
 module.exports = convertToObject;
